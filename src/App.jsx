a1bfec48acce102fb39d65c8b42decce61d5075e
artursantana <artursantana843@gmail.com>
2023-11-10 00:17:43 +0000

    New work

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
-
+import { Navbar } from "./components/Navbar"
+import { BrowserRouter as Router } from "react-router-dom"
 function App() {
 
   return (
     <>
-    <h2>Artur</h2>
+    <Router>
+      <Navbar />
+    </Router>
     </>
   )
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+
+
+export const Navbar = () => {
+    return (
+   <>
+   <Nav>
+    <NavbarContainer>
+      <NavLogo to='/'>dollars</NavLogo>
+    </NavbarContainer>
+   </Nav>
+   </>
+    )
+  }
