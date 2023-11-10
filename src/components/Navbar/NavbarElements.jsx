f6f3abcf7f1fe0fb2724e81f7277ab3fe1a76e90
artursantana <artursantana843@gmail.com>
2023-11-10 00:43:04 +0000

    New work

--- src/components/Navbar/NavbarElements.jsx
+++ src/components/Navbar/NavbarElements.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { Link as LinkR } from 'react-router-dom'
+import { Link as LinkS } from 'react-router-dom'
 
 
 export const Nav = styled.nav `
@@ -45,3 +46,47 @@ text-decoration: none;
 ` 
 
 
+export const MobileIcon = styled.div`
+display: none;
+
+@media (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+}
+`
+
+export const NavMenu = styled.ul`
+display: flex;
+align-items: center;
+list-style: none;
+text-align: center;
+margin-right: -22px;
+
+@media (max-width: 768px) {
+    display: none;
+}
+`
+
+export const NavItem = styled.li`
+height: 80px;
+`
+
+export const NavLinks = styled(LinkS)`
+color: white;
+display: flex;
+align-items: center;
+text-decoration: none;
+padding: 0 1rem;
+height: 100%;
+cursor: pointer;
+
+&active {
+    border-bottom: 3px solid #01bf71;
+}
+
+`
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,6 @@
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavLinks, NavItem } from './NavbarElements'
 
 
 export const Navbar = () => {
@@ -7,6 +9,15 @@ export const Navbar = () => {
    <Nav>
     <NavbarContainer>
       <NavLogo to='/'>dollars</NavLogo>
+      <MobileIcon>
+        <FaBars />
+      </MobileIcon>
+      <NavMenu>
+      <NavItem>
+        <NavLinks to='about'>about</NavLinks>
+      </NavItem>
+
+      </NavMenu>
     </NavbarContainer>
    </Nav>
    </>
