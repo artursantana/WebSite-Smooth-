a86b418d8208590a653653dd69836c9c2bdfca15
artursantana <artursantana843@gmail.com>
2023-11-13 22:20:32 +0000

    New work

---
+++ src/components/InfoSection/index.jsx
@@ -0,0 +1,17 @@
+
+
+export const InfoSection = () => {
+  return (
+    <>
+        <InfoContainer id={id}>
+    <InforWrapper>
+        <InfoRow>
+            <Column1>
+            
+            </Column1>
+        </InfoRow>
+    </InforWrapper>
+        </InfoContainer>
+    </>
+  )
+}
