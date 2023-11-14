1e9aa0a54af655341eccac343fef91ea48609912
artursantana <artursantana843@gmail.com>
2023-11-14 13:53:39 +0000

    New work

---
+++ src/components/InfoSection/Data.jsx
@@ -0,0 +1,16 @@
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Unlimited transactions with zero fees',
+    description: 'get access to our exclusive app that allows you to send unlimited transaction without getting charged any fees',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images'),
+    alt: 'Car',
+    dark: true,
+    primary:true,
+    darkText:false
+}
--- src/components/InfoSection/index.jsx
+++ src/components/InfoSection/index.jsx
@@ -1,26 +1,28 @@
+
+
+import { Button } from "../ButtonElements"
+
 import { InfoContainer, InfoWrapper, InfoRow, Column1, Subtitle, TextWrapper, TopLine, Heading, BtnWrap, Column2, ImgWrap, Img } from "./InfoElements"
 
-import { Button } from "react-scroll"
-
-export const InfoSection = () => {
+export const InfoSection = ({lightBg,id,imgStart, topLine, lightText, headline, darkText,buttonLabel, img, alt}) => {
   return (
     <>
-        <InfoContainer>
+        <InfoContainer lightBg={lightBg} id={id}>
     <InfoWrapper>
-        <InfoRow>
+        <InfoRow imgStart={imgStart}>
             <Column1>
             <TextWrapper>
-                <TopLine>TopLine</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{TopLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                    <Button to='home'/>
+                    <Button to='home'>{buttonLabel}</Button>
                 </BtnWrap>
             </TextWrapper>
             </Column1>
             <Column2>
                 <ImgWrap>
-                <Img />
+                <Img src={img} alt={alt} />
                 </ImgWrap>
             </Column2>
         </InfoRow>
